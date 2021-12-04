986bfea4c2a7e37558c0918dcaec5c866ce5f520
Hongbo Pang <hpang30@gatech.edu>
2021-12-04 12:18:20 +0800

    day3

--- day2/day2.py
+++ day2/day2.py
@@ -8,25 +8,20 @@ Created on Thu Dec  2 12:52:21 2021
 values = [i.strip().split() for i in open("input.txt", "r")]
 
 d1 = 0
-x1 = 0
 d2 = 0
-x2 = 0
-aim = 0
+x = 0
 
 for arg, arg2 in values:
     b = int(arg2)
     if arg == 'down':
         d1 += b
-        aim += b
     elif arg == 'up':
         d1 -= b
-        aim -= b
     else:
-        x1 += b
-        x2 += b
-        d2 += b * aim
-print(d1 * x1)
+        x += b
+        d2 += b * d1
+print(d1 * x)
 #1690020
-
-print(d2 * x2)
+       
+print(d2 * x)
 #1408487760
---
+++ day3/day3.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  3 11:04:50 2021
+
+@author: hongb
+"""
+def bin_list_to_dec(binary):
+    value = 0
+    
+    for char in binary:
+        value *= 2
+        if char == 1:
+            value += 1
+    return value
+
+values = [i.strip() for i in open("input.txt", "r")]
+length = len(values[0])
+total = 2 ** length  - 1
+
+counters = [0 for _ in range(length)]
+
+for word in values:
+    for i in range(length):
+        if word[i] == "1":
+            counters[i] += 1
+        else:
+            counters[i] -= 1
+        
+
+ans = [0 for _ in range(length)]
+
+for i in range(length):
+    if counters[i] > 0:
+        ans[i] = 1
+        
+gamma = bin_list_to_dec(ans)
+print((total - gamma) * gamma)
+#4191876
+
+bitmask = [1 for _ in range(len(values))]
+
+for i in range(length):
+    zeros = 0
+    ones = 0
+    for j in range(len(values)):
+        if bitmask[j] == 1:
+            bit = values[j][i]
+            if bit == "1":
+                ones += 1
+            else:
+                zeros += 1    
+    if zeros > ones:
+        val = "0"
+    else:
+        val = "1"
+    for j in range(len(values)):
+        if bitmask[j] == 1:
+            if values[j][i] != val:
+                bitmask[j] = 0
+    if sum(bitmask) == 1:
+       a = [int(i) for i in values[bitmask.index(1)]]
+bitmask = [1 for _ in range(len(values))]
+for i in range(length):
+    zeros = 0
+    ones = 0
+    for j in range(len(values)):
+        if bitmask[j] == 1:
+            bit = values[j][i]
+            if bit == "1":
+                ones += 1
+            else:
+                zeros += 1    
+    if zeros <= ones:
+        val = "0"
+    else:
+        val = "1"
+    for j in range(len(values)):
+        if bitmask[j] == 1:
+            if values[j][i] != val:
+                bitmask[j] = 0
+    if sum(bitmask) == 1:
+        b = [int(i) for i in values[bitmask.index(1)]]
+        
+oxy = bin_list_to_dec(a) 
+co2 = bin_list_to_dec(b)
+
+print(oxy*co2)
+#3414905
